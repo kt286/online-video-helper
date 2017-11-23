329a93a4d876ebb3ebdcfec95c253f3be97884f3
Cloud <kt286@qq.com>
2017-11-23 19:56:36 +0800

    修改代码，修复某些VIP视频网站不能加载Flash的bug

--- TencentVideo-LeTV-HTML5Play.user.js
+++ TencentVideo-LeTV-HTML5Play.user.js
@@ -1,22 +1,19 @@
 // ==UserScript==
 // @name             腾讯、乐视视频h5播放
-// @version          0.1
+// @description      腾讯、乐视视频html5播放器
+// @version          0.2
+// @author           Cloud
+// @namespace        https://github.com/kt286/TencentVideo-LeTV-HTML5Play
+// @homepageURL      https://github.com/kt286/TencentVideo-LeTV-HTML5Play
+// @supportURL       https://github.com/kt286/TencentVideo-LeTV-HTML5Play/issues
+// @updateURL        https://raw.githubusercontent.com/kt286/TencentVideo-LeTV-HTML5Play/master/TencentVideo-LeTV-HTML5Play.user.js
 // @include          *://v.qq.com/*
 // @include          *://film.qq.com/*
 // @include          *://view.inews.qq.com/*
 // @include          *://www.le.com/*
-// @author           Cloud
-// @description      腾讯、乐视视频html5播放器
 // @grant            none
 // @run-at           document-start
-// @namespace        https://greasyfork.org/users/60675
 // ==/UserScript==
-
-Object.defineProperty(navigator, 'plugins', {
-    get: function () {
-        return { length: 0 };
-    }
-});
 
 if(location.hostname=="www.le.com"){
     Object.defineProperty(navigator,"userAgent",{
@@ -25,11 +22,21 @@ if(location.hostname=="www.le.com"){
         configurable:false,
         enumerable:true
     });
-}else{
+    Object.defineProperty(navigator, 'plugins', {
+        get: function () {
+            return { length: 0 };
+        }
+    });
+}else if(location.hostname.indexOf("qq.com")!=-1){
     Object.defineProperty(navigator,"userAgent",{
-        value:"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10;  rv:48.0) Gecko/20100101 Firefox/48.0",
+        value:"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12;  rv:48.0) Gecko/20100101 Firefox/48.0",
         writable:false,
         configurable:false,
         enumerable:true
+    });
+    Object.defineProperty(navigator, 'plugins', {
+        get: function () {
+            return { length: 0 };
+        }
     });
 }
