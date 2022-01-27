6d2f012d4bd6fc296b82f3726f4c20aa461ec491
Cloud <kt286@qq.com>
2022-01-27 18:54:34 +0800

    feat: 438kan use dpalyer

--- online-video-helper.user.js
+++ online-video-helper.user.js
@@ -1,25 +1,20 @@
 // ==UserScript==
 // @name             在线视频助手
-// @description      在线视频助手：H5播放、关闭弹幕、开启宽屏(腾讯、乐视、AcFun、哔哩哔哩、爱奇艺、优酷)
+// @description      在线视频助手：H5播放、关闭弹幕、开启宽屏
 // @version          0.9
 // @author           Cloud
 // @namespace        https://github.com/kt286/online-video-helper
 // @homepageURL      https://github.com/kt286/online-video-helper
 // @supportURL       https://github.com/kt286/online-video-helper/issues
 // @updateURL        https://raw.githubusercontent.com/kt286/online-video-helper/master/online-video-helper.user.js
-// @include          *://v.qq.com/*
-// @include          *://y.qq.com/*
-// @include          *://film.qq.com/*
-// @include          *://view.inews.qq.com/*
-// @include          *://www.le.com/*
-// @include          *://*acfun.cn/*
-// @include          *://*acfun.tv/*
-// @include          *://*aixifan.com/*
-// @include          *://*.bilibili.com/video/av*
-// @include          *://*.iqiyi.com/*
-// @include          *://*.youku.com/*
+// @include          *://*.bilibili.com/video/*
+// @include          *://*.438kan.com/*
+// @include          *://438kan.com/*
 // @grant            none
-// @run-at           document-start
+// @require          https://cdn.jsdelivr.net/npm/hls.js@1.1.3/dist/hls.min.js
+// @require          https://cdn.bootcss.com/dplayer/1.26.0/DPlayer.min.js
+// @grant            GM_addStyle
+// @run-at           document-end
 // ==/UserScript==
 const host = location.hostname;
 const fakeUA = ua => Object.defineProperty(navigator, "userAgent", {
@@ -35,84 +30,63 @@ const disPlugins = () => Object.defineProperty(navigator, 'plugins', {
         };
     }
 });
+const waitForElement = (selector) => {
+    return new Promise(function (resolve, reject) {
+        let element = document.querySelector(selector);
 
-if (host == "www.le.com") {
-    fakeUA("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14) AppleWebKit/602.1.21 (KHTML, like Gecko) Version/10.2 Safari/602.1.21");
-    disPlugins();
-} else if (host.indexOf("qq.com") != -1) {
-    fakeUA("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14;  rv:48.0) Gecko/20100101 Firefox/67.0");
-    disPlugins();
-    //适配QQ音乐MV
-    localStorage.setItem('txp-user-setting', '{"isUseFlash":"0"}');
-} else if (host.indexOf("acfun.cn") != -1 || host.indexOf("acfun.tv") != -1 || host.indexOf("aixifan.com") != -1) {
-    window.onload = () => {
-        var h5script = document.createElement('script');
-        h5script.src = 'https://t5.haotown.cn/td/script.js?time=' + new Date().getTime();
-        document.body.appendChild(h5script);
-    }
-} else if (host.indexOf("bilibili.com") != -1) {
-    localStorage.setItem('defaulth5', 1);
-    window.onload = () => {
-        document.querySelector(".bilibili-player-video-btn-danmaku").click(); //关闭弹幕
-        document.querySelector(".bilibili-player-iconfont-widescreen").click(); //宽屏模式
-        var video;
-        //每秒检测video出现
-        var t = setInterval(function () {
-            video = document.querySelector(".bilibili-player-video video");
-            if (video) {
-                clearInterval(t);
-                document.querySelector(".bilibili-player-iconfont-start").click(); //自动播放
-                //document.querySelector(".bilibili-player-video").click();//自动播放
-                //document.querySelector(".bilibili-player-video video").setAttribute('autoplay', '');//自动播放
-            }
-        }, 1000);
-    }
-} else if (host.indexOf("iqiyi.com") != -1) {
-    //disPlugins();
-    document.cookie.lplayer_forcedType = 'h5_VOD';
-    // 关闭弹幕
-    window.WebSocket = function () { };
-    window.onload = () => {
-        //document.querySelector(".danmu-close .switch").click();//关闭弹幕
-        document.querySelector("[data-player-hook='webfullscreen']").click(); //网页全屏
-    }
-} else if (host.indexOf("youku.com") != -1) {
-    window.onload = () => {
-        const webfullscreenbtn = `
-        <div data-tip="网页全屏" class="control-icon control-fullscreen-icon control-webfullscreen-icon">
-             <span class="iconfont icon-quanping_normal"></span>
-        </div>`;
-        const webhalfscreenbtn = `
-        <div data-tip="退出网页全屏" class="control-icon control-halfscreen-icon control-webhalfscreen-icon" style="display:none;">
-             <span class="iconfont icon-tuichuquanping_normal"></span>
-        </div>`;
-        const webfullscreenstyle = `
-        <style>
-            .webfullscreen {
-                position: fixed;
-                z-index: 100000;
-                left: 0;
-                top: 0;
-                width: 100%;
-                height: 100%;
-            }
-        </style>`;
-        $(".h5player-dashboard .control-right-grid").append(webfullscreenbtn);
-        $(".h5player-dashboard .control-right-grid").append(webhalfscreenbtn);
-        $("head").append(webfullscreenstyle);
+        if (element) {
+            resolve(element);
+            return;
+        }
 
-        // 网页全屏
-        $(".control-webfullscreen-icon").click(function () {
-            $(".control-webhalfscreen-icon").css("display", "inline-block");
-            $(".control-webfullscreen-icon").css("display", "none");
-            $(".youku-player").toggleClass("webfullscreen");
+        let observer = new MutationObserver(function (mutations) {
+            mutations.forEach(function (mutation) {
+                let nodes = Array.from(mutation.addedNodes);
+                for (let node of nodes) {
+                    if (node.matches && node.matches(selector)) {
+                        observer.disconnect();
+                        resolve(node);
+                        return;
+                    }
+                };
+            });
         });
 
-        // 退出网页全屏
-        $(".control-webhalfscreen-icon").click(function () {
-            $(".control-webfullscreen-icon").css("display", "inline-block");
-            $(".control-webhalfscreen-icon").css("display", "none");
-            $(".youku-player").toggleClass("webfullscreen");
-        });
-    };
+        observer.observe(document.documentElement, { childList: true, subtree: true });
+    });
+}
+const click = async (target) => {
+    waitForElement(target).then((button) => {
+        button && button.click()
+    })
+}
+
+const skipChargeList = async () => {
+    const video = await waitForElement('video')
+    video && video.addEventListener('ended', async () => {
+        const jumpButton = await waitForElement('.bilibili-player-electric-panel-jump')
+        jumpButton && jumpButton.click()
+    })
+}
+
+if (host.indexOf("bilibili.com") != -1) {
+    click(".bilibili-player-video-web-fullscreen"); // 网页全屏
+} else if (host.indexOf("438kan.com") != -1) {
+    //fakeUA("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36")
+    var meta = document.createElement('meta');
+    meta.name = "referrer";
+    meta.content = "no-referrer";
+    document.getElementsByTagName('head')[0].appendChild(meta);
+
+    GM_addStyle(".MacPlayer { padding:0!important }");
+
+    // need block 438kan.com/static/player/dplayer.js
+    var dplayer = new DPlayer({
+        element: document.querySelector(".MacPlayer"),
+        autoplay: true,
+        video: {
+            url: MacPlayer.PlayUrl,
+            type: 'hls',
+        }
+    });
 }
